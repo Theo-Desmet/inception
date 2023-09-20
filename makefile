e2cb24074fea07b7fac9c05669ffbc46c51e8493
tdesmet <tdesmet@tdesmet.42.fr>
2023-09-20 05:50:03 -0400

    fix volume et db

--- makefile
+++ makefile
@@ -11,24 +11,18 @@ up:	add_volumes
 	sudo sh -c 'echo "127.0.0.1 tdesmet.42.fr" >> /etc/hosts'; \
 	docker compose -f ./srcs/docker-compose.yml up -d --build; \
 
-down:
-	@docker compose -f ./srcs/docker-compose.yml down
+stop:
+	@docker compose -f ./srcs/docker-compose.yml stop
 
 add_volumes:
 	@sudo mkdir -p /home/tdesmet/data/mariadb
-	@sudo chown -R $(shell whoami) /home/tdesmet/data/mariadb/
 	@sudo mkdir -p /home/tdesmet/data/wordpress
-	@sudo chown -R $(shell whoami) /home/tdesmet/data/wordpress/
-	@sudo touch /home/tdesmet/data/initialized
 
 clean_volumes:
 
-	@if [ -f /home/tdesmet/data/initialized ]; then \
 		docker volume rm mdb_volume wp_volume > /dev/null; \
 		sudo rm -rf /home/tdesmet/data/mariadb; \
 		sudo rm -rf /home/tdesmet/data/wordpress; \
-		sudo rm -rf /home/tdesmet/data/initialized; \
-	fi
 
 clean:
 	@docker compose -f ./srcs/docker-compose.yml down --rmi all
@@ -36,6 +30,6 @@ clean:
 	@make -s clean_volumes
 	@sudo sed -i -e '/127.0.0.1 tdesmet.42.fr/d' /etc/hosts; \
 
-re: clean all
+re: stop all
 
 
--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,21 +1,28 @@
-#!  /bin/sh
+#/bin/sh
 
-if [ ! -f /var/lib/mysql/installed ]; then
-    mariadb-install-db
-    touch /var/lib/mysql/installed
-    mariadbd & sleep 2
-    echo "creating database and user"
-    mariadb -e "USE mysql;"
-    mariadb -e "DELETE FROM mysql.user WHERE User='';"
-    mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-    mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE ${MYSQL_DB};"
-    mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'%';"
-    mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-    pkill mariadbd
-    sleep 1
+if [ -d "/var/lib/mysql/$MYSQL_DB" ]; then
+	echo "The DDB is already installed"
 else
-    echo "mariadb already installed and set up"
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --datadir=/var/lib/mysql > /dev/null
+
+	cat << EOF > request.tmp
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+
+CREATE DATABASE $MYSQL_DB CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+	mysqld --user=mysql --bootstrap < request.tmp
+	rm -f request.tmp
 fi
 
-exec $@
+exec mysqld_safe --user=mysql --console
